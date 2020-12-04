9dac7ab886f29374f58bf5b8e0f452be9b055cf9
audreyantony <web2020.audrey@gmail.com>
2020-12-04 15:12:28 +0100

    Footer

--- view/parts/dark.footer.view.php
+++ view/parts/dark.footer.view.php
@@ -1,4 +1,7 @@
-<script type="text/javascript" src="js/navigation.js"></script>
-<script type="text/javascript" src="js/darkmode.js"></script>
-</body>
+        <footer>
+            <p>Audrey Antony © 2020</p>
+        </footer>
+        <script type="text/javascript" src="js/navigation.js"></script>
+        <script type="text/javascript" src="js/darkmode.js"></script>
+    </body>
 </html>
--- view/parts/footer.view.php
+++ view/parts/footer.view.php
@@ -1,4 +1,8 @@
-<script type="text/javascript" src="js/navigation.js"></script>
-<script type="text/javascript" src="js/lightmode.js"></script>
+        <footer>
+            <p>Audrey Antony © 2020</p>
+        </footer>
+        <script type="text/javascript" src="js/navigation.js"></script>
+        <script type="text/javascript" src="js/lightmode.js"></script>
+
     </body>
 </html>
